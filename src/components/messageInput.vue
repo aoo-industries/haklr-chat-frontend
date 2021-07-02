9f6147673c2f97e2640722793b58b0caa24829fb
Matej Holubec <matej.holubec@gmail.com>
2021-07-02 09:11:22 +0200

    feat: add input

---
+++ src/components/messageInput.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <div class="input">
+      <input type="text" class="message-space" />
+      <send-icon />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import { SendIcon } from "vue-feather-icons";
+
+@Component({
+  components: {
+    SendIcon,
+  },
+})
+export default class messageInput extends Vue {}
+</script>
+
+<style lang="scss" scoped>
+.input {
+  background: white;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  width: 1178px;
+  height: 3rem;
+  position: absolute;
+
+  .message-space {
+    width: inherit;
+    height: -webkit-fill-available;
+    margin: 0 2rem 0  0
+  }
+}
+</style>
--- src/views/History.vue
+++ src/views/History.vue
@@ -1,23 +1,31 @@
 <template>
   <div class="history">
     <message-list :messages="Messages" />
+    <message-input class="bottom"/>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
-import { getMessages } from "@/services/msg.service"
+import { getMessages } from "@/services/msg.service";
 import messageList from "@/components/messageList.vue";
+import messageInput from "@/components/messageInput.vue";
 
 @Component({
   components: {
     messageList,
+    messageInput,
   },
 })
 export default class History extends Vue {
-
   get Messages() {
-    return getMessages()
+    return getMessages();
   }
 }
 </script>
+
+<style lang="scss">
+.bottom {
+  
+}
+</style>
